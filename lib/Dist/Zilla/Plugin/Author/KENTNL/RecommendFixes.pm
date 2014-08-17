6e93cf40550131d04b9e3a8abd4bdc13905d15f2
Kent Fredric <kentfredric@gmail.com>
2014-08-18 06:49:14 +1200

    v0.002001
     [Bugfix]
     - 'ok' state tracking within a few subs was wrong, @haarg++

--- lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Author::KENTNL::RecommendFixes;
 
-our $VERSION = '0.002001';
+our $VERSION = '0.002002';
 
 # ABSTRACT: Recommend generic changes to the dist.
 
