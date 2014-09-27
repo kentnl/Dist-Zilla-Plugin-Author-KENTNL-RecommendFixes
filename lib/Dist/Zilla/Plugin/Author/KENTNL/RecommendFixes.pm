beb47205d7d888c634989daf4099859101949fef
Kent Fredric <kentfredric@gmail.com>
2014-09-27 19:03:30 +1200

    v0.003006
     [Checks]
     - Make sure weaver_ini_ok runs
     - Double-check that -Encoding is not specified.

--- lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::Author::KENTNL::RecommendFixes;
 
-our $VERSION = '0.003006';
+our $VERSION = '0.003007';
 
 # ABSTRACT: Recommend generic changes to the dist.
 
