1783964da26795b91d6c3544c91e97e0c300d97b
Kent Fredric <kentfredric@gmail.com>
2015-07-12 02:30:18 +1200

    Make sure all important paths that are expected to be top-level paths match only top level paths

--- lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
@@ -246,21 +246,23 @@ _after_true contributing_pod => sub {
 
 _after_true gitignore => sub {
   my ( $rval, $self, ) = @_;
-  my $file   = $amap{'gitignore'};
-  my $assert = $self->_pc;
-  my $ok     = $rval;
-  undef $ok unless $assert->should( have_line => $file, qr/\A\.build\z/ );
-  undef $ok unless $assert->should( have_line => $file, qr/\Atmp\/\z/ );
+  my $file     = $amap{'gitignore'};
+  my $assert   = $self->_pc;
+  my $ok       = $rval;
+  my $distname = $self->zilla->name;
+  undef $ok unless $assert->should( have_line => $file, qr/\A\/\.build\z/ );
+  undef $ok unless $assert->should( have_line => $file, qr/\A\/tmp\/\z/ );
+
   if ( $self->_have_makefile_pl ) {
     ## no critic ( RegularExpressions::ProhibitFixedStringMatches )
-    undef $ok unless $assert->should( have_line => $file, qr/\AMETA\.json\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\AMYMETA\.json\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\AMETA\.yml\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\AMYMETA\.yml\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\AMakefile\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\AMakefile\.old\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\Ablib\/\z/ );
-    undef $ok unless $assert->should( have_line => $file, qr/\Apm_to_blib\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/META\.json\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/MYMETA\.json\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/META\.yml\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/MYMETA\.yml\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/Makefile\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/Makefile\.old\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/blib\/\z/ );
+    undef $ok unless $assert->should( have_line => $file, qr/\A\/pm_to_blib\z/ );
   }
   return $ok;
 };
