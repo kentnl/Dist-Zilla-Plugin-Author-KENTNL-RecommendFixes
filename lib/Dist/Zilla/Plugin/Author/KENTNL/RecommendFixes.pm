79ae6a276bde17713cbf766f7e573374e613c4af
Kent Fredric <kentfredric@gmail.com>
2016-09-10 18:31:59 +1200

    v0.005002
     [Checks]
     - Derecommend use of ConfigDumper, it was a nice idea, but the dependency and test complexity I can do without.
     - Derecommend use of dztest, again, nice idea, but unwanted complexity proliferation.
    
     [Dependencies::Stats]
     - Dependencies changed since 0.005001, see misc/*.deps* for details
     - develop: +2 ↑1 (suggests: ↑1)
    
     [Features / Win32 Ansi Color support]
     - ANSI Escape code colours should render correctly on Win32 if users have Win32::Console::ANSI installed.
     - Patched (with thanks) by Roy Ivy III ( #1 )

--- lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
+++ lib/Dist/Zilla/Plugin/Author/KENTNL/RecommendFixes.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::Author::KENTNL::RecommendFixes;
 
-our $VERSION = '0.005002';
+our $VERSION = '0.005003';
 
 # ABSTRACT: Recommend generic changes to the dist.
 
